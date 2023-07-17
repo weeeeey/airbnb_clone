64cffe9e5d437aa85860f2f08c6611ae799194b7
weeeeey <qser155@naver.com>
2023-07-18 01:28:22 +0900

    #

--- my-app/src/app/component/navbar/Categories.tsx
+++ my-app/src/app/component/navbar/Categories.tsx
@@ -1,9 +1,9 @@
-"use client";
-import { CategoryBox, Container } from "../";
-import { usePathname, useSearchParams } from "next/navigation";
+'use client';
+import { CategoryBox, Container } from '../';
+import { usePathname, useSearchParams } from 'next/navigation';
 
-import { TbBeach, TbMountain, TbPool } from "react-icons/tb";
-import { BsSnow } from "react-icons/bs";
+import { TbBeach, TbMountain, TbPool } from 'react-icons/tb';
+import { BsSnow } from 'react-icons/bs';
 import {
     GiCaveEntrance,
     GiBoatFishing,
@@ -13,101 +13,117 @@ import {
     GiForestCamp,
     GiCactus,
     GiBarn,
-} from "react-icons/gi";
-import { IoDiamond } from "react-icons/io5";
-import { MdOutlineVilla } from "react-icons/md";
-import { FaSkiing } from "react-icons/fa";
+} from 'react-icons/gi';
+import { IoDiamond } from 'react-icons/io5';
+import { MdOutlineVilla } from 'react-icons/md';
+import { FaSkiing } from 'react-icons/fa';
+import { AiOutlineRight, AiOutlineLeft } from 'react-icons/ai';
 
 export const categories = [
     {
-        label: "Beach",
+        label: 'Beach',
         icon: TbBeach,
-        description: "This property is close to the beach!",
+        description: 'This property is close to the beach!',
     },
     {
-        label: "Windmills",
+        label: 'Windmills',
         icon: GiWindmill,
-        description: "This property is has windmills!",
+        description: 'This property is has windmills!',
     },
     {
-        label: "Modern",
+        label: 'Modern',
         icon: MdOutlineVilla,
-        description: "This property is modern!",
+        description: 'This property is modern!',
     },
     {
-        label: "Countryside",
+        label: 'Countryside',
         icon: TbMountain,
-        description: "This property is in the countryside!",
+        description: 'This property is in the countryside!',
     },
     {
-        label: "Pools",
+        label: 'Pools',
         icon: TbPool,
-        description: "This is property has a beautiful pool!",
+        description: 'This is property has a beautiful pool!',
     },
     {
-        label: "Islands",
+        label: 'Islands',
         icon: GiIsland,
-        description: "This property is on an island!",
+        description: 'This property is on an island!',
     },
     {
-        label: "Lake",
+        label: 'Lake',
         icon: GiBoatFishing,
-        description: "This property is near a lake!",
+        description: 'This property is near a lake!',
     },
     {
-        label: "Skiing",
+        label: 'Skiing',
         icon: FaSkiing,
-        description: "This property has skiing activies!",
+        description: 'This property has skiing activies!',
     },
     {
-        label: "Castles",
+        label: 'Castles',
         icon: GiCastle,
-        description: "This property is an ancient castle!",
+        description: 'This property is an ancient castle!',
     },
     {
-        label: "Caves",
+        label: 'Caves',
         icon: GiCaveEntrance,
-        description: "This property is in a spooky cave!",
+        description: 'This property is in a spooky cave!',
     },
     {
-        label: "Camping",
+        label: 'Camping',
         icon: GiForestCamp,
-        description: "This property offers camping activities!",
+        description: 'This property offers camping activities!',
     },
     {
-        label: "Arctic",
+        label: 'Arctic',
         icon: BsSnow,
-        description: "This property is in arctic environment!",
+        description: 'This property is in arctic environment!',
     },
     {
-        label: "Desert",
+        label: 'Desert',
         icon: GiCactus,
-        description: "This property is in the desert!",
+        description: 'This property is in the desert!',
     },
     {
-        label: "Barns",
+        label: 'Barns',
         icon: GiBarn,
-        description: "This property is in a barn!",
+        description: 'This property is in a barn!',
     },
     {
-        label: "Lux",
+        label: 'Lux',
         icon: IoDiamond,
-        description: "This property is brand new and luxurious!",
+        description: 'This property is brand new and luxurious!',
     },
 ];
 const Categories = () => {
     const params = useSearchParams();
-    const category = params?.get("category");
+    const category = params?.get('category');
     const pathname = usePathname();
-    const isMainPage = pathname === "/";
+    const isMainPage = pathname === '/';
 
     if (!isMainPage) {
         return null;
     }
 
+    const slideOnLeft = () => {
+        const a = document.getElementById('content');
+        if (a) {
+            a.scrollLeft -= 400;
+        }
+    };
+    const slideOnRight = () => {
+        const a = document.getElementById('content');
+        if (a) {
+            a.scrollLeft += 400;
+        }
+    };
     return (
         <Container>
-            <div className="pt-4 flex flex-row items-center justify-between overflow-x-auto scrollbar-hide">
+            <div
+                id="content"
+                className="pt-4 flex flex-row items-center justify-between overflow-x-auto scrollbar-hide relative"
+            >
                 {categories.map((c) => (
                     <CategoryBox
                         key={c.label}
@@ -117,6 +133,14 @@ const Categories = () => {
                         selected={category === c.label}
                     />
                 ))}
+                <AiOutlineLeft
+                    className="text-neutral-400 hover:text-neutral-800 hover:bg-slate-200 h-full absolute left-0 hover:bg-opacity-50"
+                    onClick={slideOnLeft}
+                />
+                <AiOutlineRight
+                    className="text-neutral-400 hover:text-neutral-800 hover:bg-slate-200 h-full absolute right-0 hover:bg-opacity-50 "
+                    onClick={slideOnRight}
+                />
             </div>
         </Container>
     );
