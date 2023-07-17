ee582e2536b5e4cff01641cd0b8aeab5c7cc3c51
weeeeey <qser155@naver.com>
2023-07-17 12:23:10 +0900

    깃허브 이미지 올리는 법

--- my-app/next.config.js
+++ my-app/next.config.js
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     swcMinify: true,
+    images: {
+        domains: ["avatars.githubusercontent.com"],
+    },
 };
 
 module.exports = nextConfig;
