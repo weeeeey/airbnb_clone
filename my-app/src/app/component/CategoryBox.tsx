1070f889527a1eddea0e742c780af2fea9cd6de0
weeeeey <qser155@naver.com>
2023-07-17 14:26:53 +0900

    카테고리 박스 ui a및 선택 중인 카테고리 select 처리

--- my-app/src/app/component/CategoryBox.tsx
+++ my-app/src/app/component/CategoryBox.tsx
@@ -1,4 +1,7 @@
+import { useRouter, useSearchParams } from "next/navigation";
 import { IconType } from "react-icons";
+import { useCallback } from "react";
+import qs from "query-string";
 
 interface CategoryProps {
     label: string;
@@ -7,14 +10,47 @@ interface CategoryProps {
     selected?: boolean;
 }
 
-const CategoryBox = ({ label, description, icon, selected }: CategoryProps) => {
+const CategoryBox = ({
+    label,
+    description,
+    icon: Icon,
+    selected,
+}: CategoryProps) => {
+    const router = useRouter();
+    const params = useSearchParams();
+
+    const handleClick = useCallback(() => {
+        let currencyQuery = {};
+        if (params) {
+            currencyQuery = qs.parse(params.toString());
+        }
+        const updatedQuery: any = {
+            ...currencyQuery,
+            category: label,
+        };
+        if (params?.get("category") === label) {
+            delete updatedQuery.category;
+        }
+        const url = qs.stringifyUrl(
+            {
+                url: "/",
+                query: updatedQuery,
+            },
+            { skipNull: true }
+        );
+        router.push(url);
+    }, [label, params, router]);
+
     return (
         <div
+            onClick={handleClick}
             className={`
             ${selected ? "border-b-neutral-800" : "border-transparent"}
+            ${selected ? "text-neutral-800" : "text-neutral-500"}
             flex flex-col items-center justify-center gap-2 p-3 border-b-2 hover:text-neutral-800 transition cursor-pointer`}
         >
-            CategoryBox
+            <Icon size={26} />
+            <div className="font-medium text-sm">{label}</div>
         </div>
     );
 };
--- my-app/src/app/component/navbar/Categories.tsx
+++ my-app/src/app/component/navbar/Categories.tsx
@@ -2,6 +2,7 @@ import { CategoryBox, Container } from "../";
 import { TbBeach } from "react-icons/tb";
 import { GiWindmill } from "react-icons/gi";
 import { MdOutlineVilla } from "react-icons/md";
+import { usePathname, useSearchParams } from "next/navigation";
 
 const categories = [
     {
@@ -22,6 +23,15 @@ const categories = [
 ];
 
 const Categories = () => {
+    const params = useSearchParams();
+    const category = params?.get("category");
+    const pathname = usePathname();
+    const isMainPage = pathname === "/";
+
+    if (!isMainPage) {
+        return null;
+    }
+
     return (
         <Container>
             <div className="pt-4 flex flex-row items-center justify-between overflow-x-auto">
@@ -31,6 +41,7 @@ const Categories = () => {
                         label={c.label}
                         icon={c.icon}
                         description={c.description}
+                        selected={category === c.label}
                     />
                 ))}
             </div>
