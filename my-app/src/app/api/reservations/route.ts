21eafeb924c9c4ce444309f5d6b9013dd3dd1301
weeeeey <qser155@naver.com>
2023-07-22 16:26:17 +0900

    POST Reservation in lising

--- my-app/src/app/api/reservations/route.ts
+++ my-app/src/app/api/reservations/route.ts
@@ -7,22 +7,28 @@ import { toast } from "react-hot-toast";
 export async function POST(req: Request) {
     const currentUser = await getCurrentUser();
     if (!currentUser) {
-        return toast.error("You login account ");
+        return NextResponse.error();
     }
     const body = await req.json();
     const { totalPrice, startDate, endDate, listingId } = body;
-    try {
-        const reservation = await prisma.reservation.create({
-            data: {
-                totalPrice,
-                startDate,
-                endDate,
-                listingId,
-                userId: currentUser.id,
-            },
-        });
-        return NextResponse.json(reservation);
-    } catch (error: any) {
-        return toast.error(error);
+    if (!totalPrice || !startDate || !endDate || !listingId) {
+        return NextResponse.error();
     }
+    const listingAndReservation = await prisma.listing.update({
+        where: {
+            id: listingId,
+        },
+        data: {
+            reservations: {
+                create: {
+                    totalPrice,
+                    startDate,
+                    endDate,
+                    userId: currentUser.id,
+                },
+            },
+        },
+    });
+
+    return NextResponse.json(listingAndReservation);
 }
